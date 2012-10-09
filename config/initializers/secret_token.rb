0d220baf2e0b96e3765d792282afb12e29225b6e
Kimberley Scott <kimberley.r.scott@mac.com>
2012-10-09 12:41:13 +1000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Butter::Application.config.secret_token = '96090e9f76895b3065b7376756b74593110cf49a97900662feb75a7e537365839cea456c50f6c7af903cdb1da45ea6a3c847c52bb478b6031b26c4362bd4b28a'
